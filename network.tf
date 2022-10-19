cc46949e83810973b0ab3e3c1a1db9ee913b13a9
ramm1997 <80684861+ramm1997@users.noreply.github.com>
2022-10-19 18:01:20 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,14 +1,14 @@
 resource "azurerm_virtual_network" "example" {
   name                = var.network
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.Name
+  location            = azurerm_resource_group.example.newlocation
   address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "internal" {
   name                 = var.internal
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.example.Name
+  virtual_network_name = azurerm_virtual_network.example.Name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
