1b5c236ad7bb0e4db7986134e0808878ca737257
ramm1997 <80684861+ramm1997@users.noreply.github.com>
2022-10-19 17:58:41 +0530

    Update network_interface.tf

--- network_interface.tf
+++ network_interface.tf
@@ -1,10 +1,10 @@
 resource "azurerm_network_interface" "example" {
-  name                = "${var.name}-nic"
-  location            = "azurerm_resource_group.example.location"
-  resource_group_name = "azurerm_resource_group.example.name"
+  name                = "${var.Name}-nic"
+  location            = "azurerm_resource_group.example.newlocation"
+  resource_group_name = "azurerm_resource_group.example.Name"
 
   ip_configuration {
-    name                          = var.test
+    name                          = var.testconfig
     subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = var.private_ip
   }
